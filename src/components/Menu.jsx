a7648e08032e1b3102479f40293545dd5bc18f19
jonadepr <jonadepr@gmail.com>
2020-10-06 12:12:48 +0200

    routers 2 bottstrap

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,27 +1,53 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-import AboutMe from './AboutMe'
+import AboutMe from "./AboutMe"
 import Watercolor from "./Watercolor"
 import Contact from "./Contact"
 
 function Menu() {
   return (
-    <div>
-      <div class="dropdown">
-        <Router>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-          <button class="dropdown-item" type="button"><Link to="/about">About me</Link></button>
-          <button class="dropdown-item" type="button"><Link to="/watercolors">Watercolors</Link></button>
-          <button class="dropdown-item" type="button"><Link to="/contact">Contact</Link></button>
-        </div>
-        <Route exact path="/about" component={AboutMe}></Route>
-        <Route exact path="/watercolors" component={Watercolor}></Route>
-        <Route exact path="/contact" component={Contact}></Route>
+    <Router>
 
-        </Router>
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-    </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link"> <Link to="/about">About me</Link> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link"> <Link to="/watercolors">Watercolors</Link> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link"> <Link to="/contact">Contact</Link> <span class="sr-only">(current)</span></a>
+            </li>
+            </ul>
+
+  </div>
+</nav>
+
+         <div class="dropdown">
+          
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+
+            </div>
+            
+            <Route exact path="/about" component={AboutMe}></Route>
+            <Route exact path="/contact" component={Contact}></Route>
+            <Route exact path="/watercolors" component={Watercolor}></Route>
+
+         
+        </div> 
+
+  </Router>
+
+
+
+  
   )
 }
 
