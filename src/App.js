5e5e6573c3cddfec148ff75cc59736b018c8567b
arabbuc <abarmando@gmail.com>
2020-10-06 10:26:21 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Menu from './components/Menu'
 import Gallery from './components/Gallery'
 import Watercolor from './components/Watercolor'
+import {BrowserRouter as Router, Link, Route}  from 'react-router-dom'
 
 import './App.css';
 
